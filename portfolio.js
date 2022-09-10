fd6694e79adc12b34f3a328335f6320ea98a7b10
Aniket Gawade <aniket.gawade@spit.ac.in>
2022-09-10 12:11:19 +0530

    Resume update

--- portfolio.js
+++ portfolio.js
@@ -6,7 +6,7 @@ export const greetings = {
 	description:
 		"I am an aspiring engineer who enjoys connecting the dots: be it ideas from different disciplines people from different teams, or applications from different industries. I have strong technical skills and an academic background in engineering, object-oriented programming and machine learning. My passion lies in solving business problems with tailored data and algorithms and communicating complex ideas to non-technical stakeholders. I am able to jump across verticals to deliver high-performing solutions.",
 	resumeLink:
-		"https://drive.google.com/file/d/1WD21Y1fzGmdKY6R8lrK01X4YH9RBUxOa/view?usp=sharing",
+		"https://drive.google.com/file/d/1f0ChJ_zu57sOr4HU0qIxQGYXsP8v8M_p/view?usp=sharing",
 };
 
 export const openSource = {
